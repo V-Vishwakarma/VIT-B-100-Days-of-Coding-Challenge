87570fe5e87c19dc6f7ac7053ae34d52c4ebd772
V-Vishwakarma <144940737+V-Vishwakarma@users.noreply.github.com>
2023-12-22 02:47:13 +0530

    Completed

---
+++ Day 16/GFG.java
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+        
+        	int n = sc.nextInt();
+        
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+        
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+        
+            Solution ob = new Solution();
+            head = ob.reverseList(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   
+// } Driver Code Ends
+
+
+
+
+//function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution
+{
+    //Function to reverse a linked list.
+    Node reverseList(Node head)
+    {
+        // code here
+        Node newNode = null;
+
+        for (Node trav = head, next = null; trav != null; trav = next) {
+            
+            Node node = new Node(trav.data);
+            
+            if (newNode == null)
+                newNode = node;
+            else {
+                node.next = newNode;
+                newNode = node;
+            }
+            
+            next = trav.next;
+            
+            trav.data = 0;
+            trav.next = null;
+        }
+    
+        
+        return newNode;
+    }
+}
---
+++ Day 3/GFG.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] array = new int[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                array[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            System.out.println(sln.missingNumber(array, n));
+        }
+    }
+}
+
+
+class Solution {
+    int missingNumber(int array[], int n) {
+             int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0; i < (n - 1); i++) {
+            sum1 = sum1 + array[i];
+        }
+        sum2 = n * (n + 1) / 2;
+
+        return (sum2 - sum1);
+    }
+}
